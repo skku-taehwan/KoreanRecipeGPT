38d861652a16a209ef72d7359aebd3d7c61860d6
taehwan <74092405+skku-taehwan@users.noreply.github.com>
2021-06-16 15:24:38 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+import setuptools
+
+
+def requirements():
+    with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), encoding='utf-8') as f:
+        return f.read().splitlines()
+
+
+setuptools.setup(
+    name="ratsnlp",
+    version="0.0.9992",
+    license='MIT',
+    author="ratsgo",
+    author_email="ratsgo@naver.com",
+    description="tools for Natural Language Processing",
+    long_description=open('README.md').read(),
+    url="https://github.com/ratsgo/ratsnlp",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    package_data={
+        'ratsnlp.nlpbook.classification': ['*.html'],
+        'ratsnlp.nlpbook.ner': ['*.html'],
+        'ratsnlp.nlpbook.qa': ['*.html'],
+        'ratsnlp.nlpbook.paircls': ['*.html'],
+        'ratsnlp.nlpbook.generation': ['*.html'],
+    },
+    install_requires=requirements(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
